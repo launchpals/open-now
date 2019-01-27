7461092acccf843f19b60cb00fd970ec42765897
Robert Lin <robertlin1@gmail.com>
2019-01-26 16:47:51 -0800

    Catch interrupts, update docs

--- backend/main.go
+++ backend/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"context"
 	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/lunchpals/open-now/backend/service"
 	"go.uber.org/zap"
@@ -16,13 +18,23 @@ func main() {
 	}
 	var l = bareLogger.Sugar()
 
+	// catch interrupts
+	ctx, cancel := context.WithCancel(context.Background())
+	var signals = make(chan os.Signal)
+	signal.Notify(signals, os.Interrupt, syscall.SIGTERM, syscall.SIGINT)
+	go func() {
+		<-signals
+		cancel()
+	}()
+
+	// spin up service
 	s, err := service.New(l)
 	if err != nil {
 		println(err.Error())
 		os.Exit(1)
 	}
-	if err := s.Run(context.Background(), "127.0.0.1", "8081"); err != nil {
-		l.Fatalf("server stopped",
+	if err := s.Run(ctx, "127.0.0.1", "8081"); err != nil {
+		l.Fatalw("server stopped",
 			"error", err)
 	}
 }
